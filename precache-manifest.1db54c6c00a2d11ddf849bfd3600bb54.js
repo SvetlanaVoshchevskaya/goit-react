939f4eec378c4a6070e9efaaec8a6b0055d29648
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-05-27 21:49:50 +0300

    Updates

---
+++ precache-manifest.1db54c6c00a2d11ddf849bfd3600bb54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd7104d27e3da415a7e3fb22e71e0c6",
+    "url": "/goit-react/index.html"
+  },
+  {
+    "revision": "410e6228e5c797509a88",
+    "url": "/goit-react/static/css/main.4fc78cea.chunk.css"
+  },
+  {
+    "revision": "51626455535e526d91e4",
+    "url": "/goit-react/static/js/2.b04b51de.chunk.js"
+  },
+  {
+    "revision": "410e6228e5c797509a88",
+    "url": "/goit-react/static/js/main.267af1a7.chunk.js"
+  },
+  {
+    "revision": "162f8e5b86febdfc1b6a",
+    "url": "/goit-react/static/js/runtime~main.3c88ed6c.js"
+  }
+]);
